e91d59eedfdee45cc273ede8b38fedda25f21e6d
vasanth6190 <vasantharajan@codingmart.com>
2020-05-20 09:25:30 +0530

    basic align chnages

--- src/containers/showitems/showitems.js
+++ src/containers/showitems/showitems.js
@@ -6,9 +6,10 @@ function Showitems(props){
 const [item,setitem ] = useContext(createcontext)
 return(
     <React.Fragment>
+    
     <p className="text-center mt-5 font-weight-normal display-3" style={{ color:"#015298"}}>Our Products</p>
-    <h5 className="ml-5">Total No.of.Products: {item.length}</h5>
-<div className="row m-0 justify-content-start">
+    <h5 className="text-center">Total No.of.Products: {item.length}</h5>
+<div className="ml-3 row m-0 justify-content-start">
 {item.map((res,index)=>(
     <Card key={index} title={res.title} rating={res.rating} src={res.img} price={res.price}/>
 ))}
