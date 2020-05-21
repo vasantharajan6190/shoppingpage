f5be691c8bafacb378cb1d29bf62e8b3a7bac90e
vasanth6190 <vasantharajan@codingmart.com>
2020-05-21 11:22:33 +0530

    Updates

---
+++ precache-manifest.94b7b65ed87bbbc4f4e672aa91988875.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7372850f65019f78754e7c8147e8d547",
+    "url": "/shoppingpage/index.html"
+  },
+  {
+    "revision": "c44cc83a0b363ea868ee",
+    "url": "/shoppingpage/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "ecb4a036d23ebcbf8d20",
+    "url": "/shoppingpage/static/css/main.6e8ac6cc.chunk.css"
+  },
+  {
+    "revision": "c44cc83a0b363ea868ee",
+    "url": "/shoppingpage/static/js/2.c77d860c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/shoppingpage/static/js/2.c77d860c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecb4a036d23ebcbf8d20",
+    "url": "/shoppingpage/static/js/main.eb484412.chunk.js"
+  },
+  {
+    "revision": "679d5a2398a295f6ead6",
+    "url": "/shoppingpage/static/js/runtime-main.902441f6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/shoppingpage/precache-manifest.bbc8499b37a2322ba83bf6f9c0b94ed0.js"
+  "/shoppingpage/precache-manifest.94b7b65ed87bbbc4f4e672aa91988875.js"
 );
 
 self.addEventListener('message', (event) => {
